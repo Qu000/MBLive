5bcb8daae222f10cb02a46e40b0dbf8a7b2efbd1
jiahong <qu_jia_hong@163.com>
2018-03-08 17:31:07 +0800

    LiveView

---
+++ MBLive_iOS/MBLive_iOS/Classes/Home/Live/View/MBUserView.h
@@ -0,0 +1,13 @@
+//
+//  MBUserView.h
+//  MBLive_iOS
+//
+//  Created by qujiahong on 2018/3/8.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MBUserView : UIView
+
+@end
---
+++ MBLive_iOS/MBLive_iOS/Classes/Home/Live/View/MBUserView.m
@@ -0,0 +1,21 @@
+//
+//  MBUserView.m
+//  MBLive_iOS
+//
+//  Created by qujiahong on 2018/3/8.
+//  Copyright © 2018年 瞿嘉洪. All rights reserved.
+//
+
+#import "MBUserView.h"
+
+@implementation MBUserView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
