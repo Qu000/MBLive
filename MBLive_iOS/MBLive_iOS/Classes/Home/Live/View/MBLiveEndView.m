4614a889c46a64d20c2511587f1c3d12239a0426
jiahong <qu_jia_hong@163.com>
2018-03-12 12:01:53 +0800

    endView

--- MBLive_iOS/MBLive_iOS/Classes/Home/Live/View/MBLiveEndView.m
+++ MBLive_iOS/MBLive_iOS/Classes/Home/Live/View/MBLiveEndView.m
@@ -8,18 +8,51 @@
 
 #import "MBLiveEndView.h"
 
+@interface MBLiveEndView()
+@property (weak, nonatomic) IBOutlet UIButton *followBtn;
+@property (weak, nonatomic) IBOutlet UIButton *lookOtherBtn;
+@property (weak, nonatomic) IBOutlet UIButton *quitBtn;
+
+@end
 @implementation MBLiveEndView
 
 -(void)awakeFromNib{
     [super awakeFromNib];
     
+    [self maskRadius:self.quitBtn];
+    [self maskRadius:self.lookOtherBtn];
+    [self maskRadius:self.quitBtn];
+    
 }
+- (void)maskRadius:(UIButton *)btn
+{
+    btn.layer.cornerRadius = btn.height * 0.5;
+    btn.layer.masksToBounds = YES;
+    if (btn != self.followBtn) {
+        btn.layer.borderWidth = 1;
+        btn.layer.borderColor = KeyColor.CGColor;
+    }
+}
+
 +(instancetype)liveEndView{
     return [[NSBundle mainBundle] loadNibNamed:NSStringFromClass(self) owner:nil options:nil].lastObject;
 }
 
+- (IBAction)follow:(id)sender {
+    [sender setTitle:@"关注成功" forState:UIControlStateNormal];
+}
 
+- (IBAction)lookOther {
+    if (self.lookOtherBlock){
+        self.lookOtherBlock();
+    }
+}
 
+- (IBAction)quit {
+    if (self.quitBlock){
+        self.quitBlock();
+    }
+}
 
 
 
