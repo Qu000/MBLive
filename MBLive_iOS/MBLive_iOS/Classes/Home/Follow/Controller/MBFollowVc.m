507d3c3e4cb012d0227605669c41f5960591ffc4
jiahong <qu_jia_hong@163.com>
2018-03-13 13:46:33 +0800

    VC

--- MBLive_iOS/MBLive_iOS/Classes/Home/Follow/Controller/MBFollowVc.m
+++ MBLive_iOS/MBLive_iOS/Classes/Home/Follow/Controller/MBFollowVc.m
@@ -12,6 +12,8 @@
 
 /** 背景图 */
 @property(nonatomic, weak) UIImageView *bgImage;
+/** 提示信息 */
+@property(nonatomic, weak) UILabel *remindLab;
 
 @end
 
@@ -27,35 +29,72 @@
 - (void)setupUI{
    
     [self setupImage];
-    [self setupRemindLab];
+
 }
 /** 背景图片 */
 - (void)setupImage{
     
     UIImageView * bgImage = [[UIImageView alloc]init];
     bgImage.image = [UIImage imageNamed:@"no_follow_250x247"];
-    [bgImage mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(@96);
-        make.width.height.equalTo(@250);
-        make.centerX.equalTo(self.view);
-    }];
+    CGFloat WH = 250;
+    CGFloat Y = 96;
+    CGFloat X = (self.view.width - WH)/2;
+    bgImage.frame = CGRectMake(X, Y, WH, WH);
     [self.view addSubview:bgImage];
     self.bgImage = bgImage;
+    /*
+     [bgImage mas_makeConstraints:^(MASConstraintMaker *make) {
+     make.top.equalTo(@96);
+     make.width.height.equalTo(@250);
+     make.centerX.equalTo(self.view);
+     }];
+     */
+    [self setupRemindLab];
     
 }
+
+
 /** 提示信息 */
 - (void)setupRemindLab{
     UILabel * remindLab = [[UILabel alloc]init];
     remindLab.text = @"你关注的主播还没有开播呐";
     remindLab.textColor = [UIColor whiteColor];
     remindLab.textAlignment = NSTextAlignmentCenter;
-    remindLab.font = [UIFont systemFontOfSize:15];
+    remindLab.font = [UIFont systemFontOfSize:18];
     remindLab.backgroundColor = [UIColor clearColor];
-    [remindLab mas_makeConstraints:^(MASConstraintMaker *make) {
-        make.top.equalTo(self.bgImage);
-        make.left.right.equalTo(@30);
-        make.height.equalTo(@22);
-    }];
+    CGFloat M = 30;
+    CGFloat W = self.view.width - 2*M;
+    CGFloat Y = CGRectGetMaxY(self.bgImage.frame)+30;
+    CGFloat H = 22;
+    remindLab.frame = CGRectMake(M, Y, W, H);
     [self.view addSubview:remindLab];
+    self.remindLab = remindLab;
+    /*
+     [remindLab mas_makeConstraints:^(MASConstraintMaker *make) {
+     make.top.equalTo(self.bgImage);
+     make.left.right.equalTo(@30);
+     make.height.equalTo(@22);
+     }];
+     */
+    
+    [self setupSeeBtn];
+}
+- (void)setupSeeBtn{
+    UIButton * seeBtn = [[UIButton alloc]init];
+    [seeBtn setTitle:@"去看看当前热门直播" forState:UIControlStateNormal];
+    seeBtn.tintColor = [UIColor whiteColor];
+    seeBtn.backgroundColor = [UIColor clearColor];
+    seeBtn.titleLabel.textAlignment = NSTextAlignmentCenter;
+    [seeBtn addTarget:self action:@selector(clickSeeBtn) forControlEvents:UIControlEventTouchUpInside];
+    seeBtn.titleLabel.font = [UIFont systemFontOfSize:18];
+    CGFloat M = 30;
+    CGFloat W = self.view.width - 2*M;
+    CGFloat Y = CGRectGetMaxY(self.remindLab.frame)+30;
+    CGFloat H = 22;
+    seeBtn.frame = CGRectMake(M, Y, W, H);
+    [self.view addSubview:seeBtn];
+}
+- (void)clickSeeBtn{
+    [[NSNotificationCenter defaultCenter] postNotificationName:kNotifyToseeBigWorld object:nil];
 }
 @end
